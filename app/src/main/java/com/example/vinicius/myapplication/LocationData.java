2aa2abe83b5916c91e15c0a3162dfd0e80533743
irrlight <mvsl072@gmail.com>
2019-02-03 23:34:38 -0200

    first commit

---
+++ app/src/main/java/com/example/vinicius/myapplication/LocationData.java
@@ -0,0 +1,12 @@
+package com.example.vinicius.myapplication;
+
+public class LocationData {
+
+    double latitude;
+    double longitude;
+
+    public LocationData(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
